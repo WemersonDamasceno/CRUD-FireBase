7e7a8f78dda02084aad55d8519cd49aa2b50a84a
Wemerson7 <wemerson_damasceno@yahoo.com>
2020-02-11 12:19:57 -0300

    Foi adicionado a possibilidade do usuario enviar uma foto, tirada pela camera ou escolhendo da galeria, e salva-la no banco de dados.

--- app/src/main/java/com/example/testebancodadosfirebase/MainActivity.java
+++ app/src/main/java/com/example/testebancodadosfirebase/MainActivity.java
@@ -2,24 +2,46 @@ package com.example.testebancodadosfirebase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.testebancodadosfirebase.DAO.PessoaDAO;
 import com.example.testebancodadosfirebase.model.Pessoa;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +56,24 @@ public class MainActivity extends AppCompatActivity {
     Pessoa pessoaSelected;
     PessoaDAO pessoaDAO;
 
+    final int GALERIA = 1;
+    final int CAMERA = 3;
+    final int PERMISSAO_REQUEST = 2;
+    //imagens
+    Button btGaleria;
+    Uri selectedImage;
+    Button btCamera;
+    ImageView imgView;
+    ImageView imgViewList;
+
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    //imagem BD
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +85,23 @@ public class MainActivity extends AppCompatActivity {
         listViewDados = findViewById(R.id.listViewDados);
         pessoaDAO = new PessoaDAO();
 
+
+        //imagens
+        btCamera = findViewById(R.id.btCamera);
+        btGaleria = findViewById(R.id.btGaleria);
+        imgView = findViewById(R.id.imgView);
+        imgViewList = findViewById(R.id.imgViewList);
+
+
         //Inicializando o banco de dados
         inicializandoFirebase();
         pegarDadosDataBase();
+        //inicializando o firebase storage
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        //permissao do usuario
+        usuarioPermission();
+
 
         listPessoas = new ArrayList<Pessoa>();
 
@@ -63,6 +115,39 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        btGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALERIA);
+            }
+        });
+
+        btCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, CAMERA);
+                }
+            }
+        });
+
+
+    }
+
+    private void usuarioPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PERMISSAO_REQUEST);
+            }
+        }
     }
 
     private void pegarDadosDataBase() {
@@ -107,10 +192,43 @@ public class MainActivity extends AppCompatActivity {
             pessoa.setNome(editNome.getText().toString());
             pessoa.setEmail(editEmail.getText().toString());
 
-            //teste DAO
             pessoaDAO.salvarPessoaBD(pessoa, MainActivity.this);
             //databaseReference.child("Pessoa").child(pessoa.getId()).setValue(pessoa);
+
+            //salvar imagem no banco de dados FireBase;
+            if (selectedImage != null) {
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle("Uploading...");
+                progressDialog.show();
+
+                StorageReference ref = storageReference.child("images/" + pessoa.getId());
+                ref.putFile(selectedImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        progressDialog.setTitle("Uploaded " + (int) progress + "%");
+                    }
+                });
+
+            }
+
+
+
+
             limparCampos();
+
 
         } else if (id == R.id.menu_atualizar) {
             Pessoa p = new Pessoa();
@@ -144,4 +262,46 @@ public class MainActivity extends AppCompatActivity {
         editEmail.setText("");
         editNome.setText("");
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //acessar galeria
+        if (resultCode == RESULT_OK && requestCode == GALERIA) {
+            selectedImage = data.getData();
+            String[] filePath = {MediaStore.Images.Media.DATA};
+            assert selectedImage != null;
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+            assert c != null;
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            imgView.setImageBitmap(thumbnail);
+        }
+        //acessar camera
+        if (requestCode == CAMERA && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imageBitmap);
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSAO_REQUEST) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // A permissão foi concedida. Pode continuar
+            } else {
+                // A permissão foi negada. Precisa ver o que deve ser desabilitado
+            }
+            return;
+        }
+    }
+
+
 }
